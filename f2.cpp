5053a17687d5c38d6b6704d639ac14cdea0256df
unknown <omrajput490@gmail.com>
2024-02-16 15:56:08 -0500

    final code

--- f2.cpp
+++ f2.cpp
@@ -1,4 +1,12 @@
-
+/*
+* FILE : f2.CPP
+* PROJECT : SENG1000 - Focused Assignment #2
+* PROGRAMMER : Aumkumar Parmar
+* Student id:8982250
+* FIRST VERSION : 2024-01-24
+* DESCRIPTION :
+* The function in this file are used to add integers from 1 to 500 using while loop and display sum after the loop is completed.
+*/
 #include <stdio.h>
 
 int main(void)
